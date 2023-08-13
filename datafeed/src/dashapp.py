f72db96e153070bc04367efc923927e376e43506
kevin george <ksgeorge@uci.edu>
2023-08-13 00:20:06 -0700

    remove github

--- datafeed/src/dashapp.py
+++ datafeed/src/dashapp.py
@@ -161,7 +161,18 @@ def main():
         visibility: hidden;
         }
         """
-    st.markdown(hide, unsafe_allow_html=True)
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
     # if not "sleep_time" in st.session_state:
     #     st.session_state.sleep_time = 5
 
