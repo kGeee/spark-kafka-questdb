b9661581658462286fdb12c7160f8c94465ece5e
kevin george <ksgeorge@uci.edu>
2023-08-13 17:50:42 -0700

    css modify

--- datafeed/src/dashapp.py
+++ datafeed/src/dashapp.py
@@ -161,7 +161,7 @@ def main():
     hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
-        .css-q16mip {visibility: hidden;}
+        .css-zq5wmm {visibility: hidden;}
         </style>
         """
     st.markdown(hide_menu_style, unsafe_allow_html=True)        
